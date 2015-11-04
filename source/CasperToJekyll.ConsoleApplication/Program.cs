ca0157fecce06532c51ccfb9baea00cada18c3ea
Tim Murphy <tim@26tp.com>
2015-11-05 08:41:56 +1100

    Add console project

---
+++ source/CasperToJekyll.ConsoleApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CasperToJekyll.ConsoleApplication
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ source/CasperToJekyll.ConsoleApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CasperToJekyll.ConsoleApplication")]
+[assembly: AssemblyDescription("Convert Casper based content repository to Jekyll based.")]
+[assembly: AssemblyProduct("CasperToJekyll")]
+[assembly: AssemblyCopyright("Copyright © 2015 Tim Murphy")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
